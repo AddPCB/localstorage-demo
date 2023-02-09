64cd5e5bfb5fb0eaf7053401a07a94b0ad20d093
Peter Brightmore <122779059+AddPCB@users.noreply.github.com>
2023-02-09 23:52:17 +0000

    Update app.js
    apply the ccs rule and html onclick attributes from within JS, because the scope of the exercise was to contain logic in the JS file.
    technically correct code is correct code

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ function setTheme() {
   // change theme from dark to light  and vice versa, depending what it is currently set to
 
   if (theme === "light") {
-    localStorage.setItem("theme", "dark");
+    localStorage.setItem("theme", "dark")
+// apply the dark class
     document.body.setAttribute("class", "dark");
   } 
   else {
@@ -20,4 +21,9 @@ function setTheme() {
 function getTheme() {
   theme = localStorage.getItem("theme");
 }
+// populate 'theme' by running the 'else' path of setTheme 
 setTheme();
+// Create a new class called dark to invert background colour
+document.styleSheets[0].insertRule(".dark { background-color: rgba(255, 255, 255, 0.5); filter: invert(1); }", 0);
+// hook setTheme to the first button on the page
+document.body.button.setAttribute("onClick", "setTheme()";
